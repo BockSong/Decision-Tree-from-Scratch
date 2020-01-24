e6fc641fcc48d6d5a2cebda334a297bd6bf96a4e
BockSong <songrcs@gmail.com>
2020-01-24 18:02:44 -0500

    fix bugs in inspection.py

--- inspection.py
+++ inspection.py
@@ -3,29 +3,19 @@ import numpy as np
 
 Debug = True
 
-# return the y value which appears most times
-def majority_vote(dataset):
-    cnt = dict()
-    for data in dataset:
-        if data[-1] in cnt:
-            cnt[data[-1]] += 1
-        else:
-            cnt[data[-1]] = 1
-    return max(cnt, key = lambda x: cnt[x])
-    
 def inspect(input_file):
-    left_count, right_count = 0, 0
     value_left = None
+    left_count, right_count = 0., 0.
 
     with open(input_file, 'r') as f:
-        idx = 0
+        idx = -1
         for line in f:
-            if idx == 0:
+            if idx == -1:
                 idx += 1
                 continue
             split_line = line.strip().split('\t')
             # set the first y value met as for left edge
-            if idx == 1:
+            if idx == 0:
                 value_left = split_line[-1]
 
             if split_line[-1] == value_left:
@@ -38,7 +28,7 @@ def inspect(input_file):
     error = min(left_count, right_count) / idx
 
     if Debug:
-        print("Dataset size: ", idx - 1)
+        print("Dataset size: ", idx)
         print("For value ", value_left, ": ", left_count)
         print("For the other value: ", right_count)
         print("gini_impurity: " + str(gini))
